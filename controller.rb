116fdb1b13e573a6215f4cc17aa4fce9834fbb48
Yi Jie Ng ngyijie92@gmail.com <ngyijie92@gmail.com>
2021-09-23 18:27:08 +0800

    Defined instance method to edit existing entry

--- controller.rb
+++ controller.rb
@@ -44,6 +44,90 @@ class Controller
     display_crystals
   end
 
+  # Edit name
+  def edit_name!
+    # 1. List all crystals
+    display_crystals
+    # 2. Ask user for crystal id
+    id = @view.ask_id
+    # 3. Find crystal in repo
+    crystal = @crystals.find(id)
+    # 4. Ask user for new name
+    new_name = @view.ask_name
+    # 5. Update crystal to new name
+    crystal.instance_variable_set(:@name, new_name)
+    # 6. Update list of crystals
+    @crystals.save_csv
+  end
+
+  # Edit type
+  def edit_type!
+    # 1. List all crystals
+    display_crystals
+    # 2. Ask user for crystal id
+    id = @view.ask_id
+    # 3. Find crystal in repo
+    crystal = @crystals.find(id)
+    # 4. Ask user for new type
+    new_type = @view.ask_type
+    # 5. Update crystal to new name
+    crystal.instance_variable_set(:@type, new_type)
+    # 6. Update list of crystals
+    @crystals.save_csv
+  end
+
+  # Edit shop
+  def edit_shop!
+    # 1. List all crystals
+    display_crystals
+    # 2. Ask user for crystal id
+    id = @view.ask_id
+    # 3. Find crystal in repo
+    crystal = @crystals.find(id)
+    # 4. Ask user for new shop
+    new_shop = @view.ask_shop
+    # 5. Update crystal to new name
+    crystal.instance_variable_set(:@shop, new_shop)
+    # 6. Update list of crystals
+    @crystals.save_csv
+  end
+
+  # Edit price
+  def edit_price!
+    # 1. List all crystals
+    display_crystals
+    # 2. Ask user for crystal id
+    id = @view.ask_id
+    # 3. Find crystal in repo
+    crystal = @crystals.find(id)
+    # 4. Ask user for new price
+    new_price = @view.ask_price
+    # 5. Update crystal to new name
+    crystal.instance_variable_set(:@price, new_price)
+    # 6. Update list of crystals
+    @crystals.save_csv
+  end
+
+  # Edit existing entry
+  def edit!
+    # 1. Ask user if want to edit name/type/shop/price
+    user_edit = @view.ask_edit
+    case user_edit
+    # 2a. When name, update name
+    when "name" then edit_name!
+    # 2b. When type, update type
+    when "type" then edit_type!
+    # 2c. When shop, update shop
+    when "shop" then edit_shop!
+    # 2d. When price, update price
+    when "price" then edit_price!
+    else
+      puts "Name/Type/Shop/Price?"
+    end
+    # 3. List updated crystals
+    display_crystals
+  end
+
   private
 
   def display_crystals
