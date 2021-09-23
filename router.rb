31d796aa7bcd7a6b137f4324f830dda34e699f30
Yi Jie Ng ngyijie92@gmail.com <ngyijie92@gmail.com>
2021-09-23 18:48:03 +0800

    Defined instance methods to route to controller actions

--- router.rb
+++ router.rb
@@ -3,4 +3,45 @@ class Router
     @controller = controller
     @running = true
   end
+
+  def run
+    puts "----------------------"
+    puts "My Crystals Diary App"
+    puts "----------------------"
+
+    while @@running
+      display_actions
+      action = gets.chomp.to_i
+      print `clear`
+      route_action(action)
+    end
+  end
+
+  private
+
+  def route_action(action)
+    case action
+    when 1 then controller.list
+    when 2 then controller.create
+    when 3 then controller.edit!
+    when 4 then controller.destroy!
+    when 5 then controller.stop
+    else
+      puts "Please select 1/2/3/4/5"
+    end
+  end
+
+  def stop
+    @running = false
+    puts "Bye!"
+  end
+
+  def display_actions
+    puts ""
+    puts "1 - List all crystals"
+    puts "2 - Add a new crystal"
+    puts "3 - Update existing crystal"
+    puts "4 - Remove a crystal"
+    puts "5 - Stop and exit the app"
+  end
 end
