e9f290599505f38b6e9a070327440019bdb1c3b5
Yi Jie Ng ngyijie92@gmail.com <ngyijie92@gmail.com>
2021-09-23 17:56:22 +0800

    Defined instance methods to ask user for name, type, shop and price

--- view.rb
+++ view.rb
@@ -4,4 +4,28 @@ class View
       puts "#{crystal.id}. #{crystal.name}, Type: #{crystal.type}, Shop: #{crystal.shop}, $#{crystal.price}"
     end
   end
+
+  def ask_name
+    puts "Crystal name?"
+    print ">"
+    gets.chomp
+  end
+
+  def ask_type
+    puts "Crystal type?"
+    print ">"
+    gets.chomp
+  end
+
+  def ask_shop
+    puts "Claimed from which shop?"
+    print ">"
+    gets.chomp
+  end
+
+  def ask_price
+    puts "Price?"
+    print ">"
+    gets.chomp.to_i
+  end
 end
