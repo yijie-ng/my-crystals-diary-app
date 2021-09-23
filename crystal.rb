08ab6889f0333b4f11f93b6f0a7475dfcb600450
Yi Jie Ng ngyijie92@gmail.com <ngyijie92@gmail.com>
2021-09-23 16:40:51 +0800

    Created crystal model

---
+++ crystal.rb
@@ -0,0 +1,9 @@
+class Crystal
+  def initialize(attributes = {})
+    @id = attributes[:id]
+    @name = attributes[:id]
+    @type = attributes[:type]
+    @shop = attributes[:shop]
+    @price = attributes[:price]
+  end
+end
