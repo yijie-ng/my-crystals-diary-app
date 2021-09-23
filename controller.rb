453ac5d49f9d5c8247fa13e60b7bcbbc667e89c6
Yi Jie Ng ngyijie92@gmail.com <ngyijie92@gmail.com>
2021-09-23 17:52:24 +0800

    Defined an instance method to create new crystal

--- controller.rb
+++ controller.rb
@@ -12,6 +12,24 @@ class Controller
     display_crystals
   end
 
+  # Add new crystal
+  def create
+    # 1a. Ask user for crystal name
+    name = @view.ask_name
+    # 1b. Ask user for crystal type
+    type = @view.ask_type
+    # 1c. Ask user for shop name
+    shop = @view.ask_shop
+    # 1d. Ask user for crystal price
+    price = @view.ask_price
+    # 2. Create new crystal
+    crystal = Crystal.new(name: name, type: type, shop: shop, price: price)
+    # 3. Add to repo
+    @crystals.add(crystal)
+    # 4. Display updated list
+    display_crystals
+  end
+  
   private
 
   def display_crystals
