2b4e94b6c1f9855342d616af0b36e91e2796334c
Yi Jie Ng ngyijie92@gmail.com <ngyijie92@gmail.com>
2021-09-23 17:41:35 +0800

    Defined method to display list of crystals

--- view.rb
+++ view.rb
@@ -1,2 +1,7 @@
 class View
+  def display(crystals)
+    crystals.each do |crystal|
+      puts "#{crystal.id}. #{crystal.name}, Type: #{crystal.type}, Shop: #{crystal.shop}, $#{crystal.price}"
+    end
+  end
 end
