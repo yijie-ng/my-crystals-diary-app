0af080b8a7765fdd942d90c8a67dc2a103ff8ea8
Yi Jie Ng ngyijie92@gmail.com <ngyijie92@gmail.com>
2021-09-23 17:04:39 +0800

    Resolved name instance

--- crystal.rb
+++ crystal.rb
@@ -1,10 +1,10 @@
 class Crystal
   attr_reader :name, :type, :shop, :price
   attr_accessor :id
-  
+
   def initialize(attributes = {})
     @id = attributes[:id]
-    @name = attributes[:id]
+    @name = attributes[:name]
     @type = attributes[:type]
     @shop = attributes[:shop]
     @price = attributes[:price]
